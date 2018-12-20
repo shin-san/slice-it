70ea2f1f9a7fe8c2bcedca126fc09bedfd54d506
John Chan <chan.j@heritage.com.au>
2018-12-20 12:58:31 +1000

    Initial Commit

--- src/main/java/au/com/advent/SliceIt.java
+++ src/main/java/au/com/advent/SliceIt.java
@@ -1,5 +1,13 @@
 package au.com.advent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SliceIt {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(SliceIt.class);
+
+    public static void main(String[] args) {
+
+    }
 }
